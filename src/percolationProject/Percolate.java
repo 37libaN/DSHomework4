9ae3cec97389dfab0b90ef400cf12c10fd7eeb74
Vishnu <vishnuappalaraju@gmail.com>
2018-09-21 11:51:23 -0500

    stuff

---
+++ src/percolationProject/Percolate.java
@@ -0,0 +1,5 @@
+package percolationProject;
+
+public class Percolate {
+
+}
--- src/percolationProject/RandomGrid.java
+++ src/percolationProject/RandomGrid.java
@@ -1,5 +1,42 @@
 package percolationProject;
-
+import java.util.*;
 public class RandomGrid {
+	private int[][] randomgrid;
+	private int rannum;
+	public RandomGrid(int n, double p) {
+		Random random = new Random();
+		randomgrid = new int[n][n];
+		for(int r = 0; r < randomgrid.length; r++) {
+			for(int c = 0; c < randomgrid[n].length; c++) {
+				rannum = random.nextInt();
+				if(rannum <= p) {
+					randomgrid[r][c] = 1;
+				}
+				else {
+					randomgrid[r][c] = 0;
+				}
+			}
+		}
+	}
+	
+	public int getValue(int row, int col) {
+		return randomgrid[row][col];
+	}
+	
+	public void setValue(int row, int col, int value) {
+		
+	}
+
+	public int getSize() {
+		
+	}
+
+	public String toString() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
 
 }
