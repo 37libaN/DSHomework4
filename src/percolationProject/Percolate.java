2c71253987794eb4efd56e744c68137add4481c9
Vishnu <vishnuappalaraju@gmail.com>
2018-09-22 15:26:34 -0500

    litty memes

--- src/percolationProject/Percolate.java
+++ src/percolationProject/Percolate.java
@@ -1,5 +1,45 @@
 package percolationProject;
 
-public class Percolate {
+import java.util.*;
 
+public class Percolate {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String theinput = input.nextLine();
+		String valueN = theinput.substring(0, theinput.indexOf(" "));
+		int n = Integer.parseInt(valueN);
+		String valueP = theinput.substring(theinput.indexOf(" ") + 1);
+		double p = Double.parseDouble(valueP);
+		RandomGrid rg = new RandomGrid(n, p);
+		percolate(0, 0, rg);
+		int lastRow = rg.getSize() - 1;
+		boolean percolated = false;
+		while (!percolated) {
+			for (int i = 0; i < rg.getSize(); i++) {
+				if (rg.getValue(lastRow, i) > 1) {
+					percolated = true;
+				}
+			}
+		}
+		System.out.println(rg.toString());
+		System.out.println("Percolated = " + percolated);
+	}
+
+	public static void percolate(int r, int c, RandomGrid g) {
+		if (g.getValue(r, c) == 0) {
+			g.setValue(r, c, 2);
+		}
+		if (c + 1 >= 0) {
+			percolate(r, c + 1, g);
+		}
+		if (r + 1 >= 0) {
+			percolate(r + 1, c, g);
+		}
+		if (c - 1 >= 0) {
+			percolate(r, c - 1, g);
+		}
+		if (r - 1 >= 0) {
+			percolate(r - 1, c, g);
+		}
+	}
 }
