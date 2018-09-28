561ec349f78d7107f7b5f6cf5722ba333228f9ee
Unknown <20hussan@elmbrookstudents.org>
2018-09-28 11:51:20 -0500

    Diff fluid methods written

--- src/percolationProject/Percolate2.java
+++ src/percolationProject/Percolate2.java
@@ -10,18 +10,50 @@ package percolationProject;
 import java.util.*;
 
 public class Percolate2 {
-	public void percolateFluidType1(){
-		
+
+	public void percolateFluidType1(int r, int c, RandomGrid g) { // percolates vertically
+																	
+		if (g.getValue(r, c) == 0) {
+			g.setValue(r, c, 2);
+		}
+		if (!(r + 1 > (g.getSize() - 1))) {
+			if (g.getValue(r + 1, c) == 0)
+				percolate(r + 1, c, g);
+		}
+		if (r - 1 >= 0) {
+			if (g.getValue(r - 1, c) == 0)
+				percolate(r - 1, c, g);
+		}
+		return;
 	}
-	public void percolateFluidType2(){
+
+	public void percolateFluidType2(int r, int c, RandomGrid g) { // percolates horizontally and vertically																	
 		
+		if (g.getValue(r, c) == 0) {
+			g.setValue(r, c, 2);
+		}
+
+		if (!(c + 1 > (g.getSize() - 1))) {
+			if (g.getValue(r, c + 1) == 0)
+				percolate(r, c + 1, g);
+		}
+		if (!(r + 1 > (g.getSize() - 1))) {
+			if (g.getValue(r + 1, c) == 0)
+				percolate(r + 1, c, g);
+		}
+		if (c - 1 >= 0) {
+			if (g.getValue(r, c - 1) == 0)
+				percolate(r, c - 1, g);
+		}
+		if (r - 1 >= 0) {
+			if (g.getValue(r - 1, c) == 0)
+				percolate(r - 1, c, g);
+		}
+		return;
 	}
-	public void percolateFluidType3(){
+
+	public void percolateFluidType3(int r, int c, RandomGrid g) { //percolates vertically, horizontally, and diagonally																
 		
-	}
-	
-	
-	public static void percolate(int r, int c, RandomGrid g) { //percolates through array
 		if (g.getValue(r, c) == 0) {
 			g.setValue(r, c, 2);
 		}
@@ -42,6 +74,49 @@ public class Percolate2 {
 			if (g.getValue(r - 1, c) == 0)
 				percolate(r - 1, c, g);
 		}	
+		
+		//diagonals
+		if (!(c + 1 > (g.getSize() - 1)) && !(r + 1 > (g.getSize() - 1))) {
+			if (g.getValue(r + 1, c + 1) == 0)
+				percolate(r + 1, c + 1, g);
+		}
+		if (!(c + 1 > (g.getSize() - 1)) && (r - 1 >= 0)) {
+			if (g.getValue(r - 1, c + 1) == 0)
+				percolate(r + 1, c, g);
+		}
+		if ((c - 1 >= 0) && !(r + 1 > (g.getSize() - 1))) {
+			if (g.getValue(r + 1, c - 1) == 0)
+				percolate(r + 1, c - 1, g);
+		}
+		if ((c - 1 >= 0) && (r - 1 >= 0)) {
+			if (g.getValue(r - 1, c - 1) == 0)
+				percolate(r - 1, c - 1, g);
+		}	
+		return;
+	}
+
+	public static void percolate(int r, int c, RandomGrid g) { // percolates
+																// through array
+		if (g.getValue(r, c) == 0) {
+			g.setValue(r, c, 2);
+		}
+
+		if (!(c + 1 > (g.getSize() - 1))) {
+			if (g.getValue(r, c + 1) == 0)
+				percolate(r, c + 1, g);
+		}
+		if (!(r + 1 > (g.getSize() - 1))) {
+			if (g.getValue(r + 1, c) == 0)
+				percolate(r + 1, c, g);
+		}
+		if (c - 1 >= 0) {
+			if (g.getValue(r, c - 1) == 0)
+				percolate(r, c - 1, g);
+		}
+		if (r - 1 >= 0) {
+			if (g.getValue(r - 1, c) == 0)
+				percolate(r - 1, c, g);
+		}
 		return;
 	}
 }
