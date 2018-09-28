4735dd849e96bbe0bfcee518c9668729f0437d99
Unknown <20hussan@elmbrookstudents.org>
2018-09-28 11:25:23 -0500

    actual input in percrunner

--- src/percolationProject/PercRunner.java
+++ src/percolationProject/PercRunner.java
@@ -1,10 +1,17 @@
 package percolationProject;
 
+import java.util.Scanner;
+
 public class PercRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		int gridSize = input.nextInt();
+		int runs = input.nextInt();
+		int granularity = input.nextInt();
+		int threads = input.nextInt();
+		int fluidType = input.nextInt();
 	}
 
 }
