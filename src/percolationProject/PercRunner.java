a6eb74daf13e87644a33020315b584aaae310ed3
Vishnu <vishnuappalaraju@gmail.com>
2018-09-30 21:11:33 -0500

    Confusion, READ DESCRIPTION
    I'm still confused on how to run concurrent threads with different values when we only take in the input once. Also how do we get the value p, considering the user doesn't input it?????????????????????????????

--- src/percolationProject/PercRunner.java
+++ src/percolationProject/PercRunner.java
@@ -6,14 +6,14 @@ public class PercRunner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);
 		int gridSize = input.nextInt();
 		int runs = input.nextInt();
 		int granularity = input.nextInt();
 		int threads = input.nextInt();
-		int fluidType = input.nextInt();*/
+		int fluidType = input.nextInt();
 		Percolate2 perc = new Percolate2();
-		perc.runPercolate(1, 5, 0.5);
+		perc.runPercolate(fluidType, gridSize, 0.5);
 		perc.runPercolate(2, 5, 0.5);
 		perc.runPercolate(3, 5, 0.5);
 	}
