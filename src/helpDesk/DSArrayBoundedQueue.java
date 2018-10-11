a0c2dea9e21a4832de7712da4bdb0e22aca35514
Vishnu <vishnuappalaraju@gmail.com>
2018-10-10 19:33:45 -0500

    fdcgvj

--- src/helpDesk/DSArrayBoundedQueue.java
+++ src/helpDesk/DSArrayBoundedQueue.java
@@ -1,3 +1,10 @@
+/**
+	 * COSC 2100 - Project 6
+	 * A program simulating a helpdesk for students; this time it implements a waiting area as well.
+	 * @author [Vishnu Appalaraju, Nabil Hussaini]
+	 * Instructor [Dr. Brylow] 
+	 * TA-BOT:MAILTO [srivishnu.appalaraju@marquette.edu nabil.hussaini@marquette.edu] 
+	 */
 
 public class DSArrayBoundedQueue<T> implements DSQueueInterface<T>
 {
