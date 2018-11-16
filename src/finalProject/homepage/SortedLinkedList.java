ef4113d9ae7619ae8277e06a26c09056c23d100c
Unknown <20hussan@elmbrookstudents.org>
2018-11-16 00:00:09 -0600

    added in sortedlinkedlist

---
+++ src/finalProject/homepage/SortedLinkedList.java
@@ -0,0 +1,68 @@
+package finalProject.homepage; 
+ 
+public class SortedLinkedList<T> { 
+	private int numElements; //number of elements in list 
+	private Node<T> list; //1st node in list 
+	private Node<T> loc; //where you are in list when going through it 
+	private Node<T> prev; //one node before loc, null if loc is first element 
+	private Node<T> foundNode; //node that is found by find 
+	public SortedLinkedList(){ 
+		list = null; 
+		loc = null; 
+		prev = null; 
+	} 
+	public boolean isEmpty(){ //returns true if empty, false if not 
+		if (list == null)
+			return true;
+		return false;
+	} 
+	public void add(T element){ //adds an element into its sorted position in list, sorted by ascending 
+		reset(); 
+		T currElement; //current element used for comparisons 
+		if(loc == null) 
+			return; 
+		while(loc!=null){ 
+			currElement = loc.getInfo(); 
+			if(!(((Comparable) element).compareTo(currElement)<0)){  
+				step(); //keep going through list until element to add is less than currElement 
+			}	 
+			else 
+				break;		 
+		} 
+		Node<T> toAdd = new Node<T>(element); 
+		if(prev == null){//if new node should be first Node 
+			toAdd.setLink(list); 
+			list = toAdd; 
+		} 
+		else{//inserts new node somewhere else in list 
+			toAdd.setLink(loc); 
+			prev.setLink(toAdd); 
+		} 
+	} 
+	public Node<T> getNode(){ //returns loc 
+		return loc; 
+	} 
+	public void step(){ //advances loc by one element 
+		prev = loc; 
+		loc = loc.getLink(); 
+	} 
+	public void reset(){ //resets loc to first element 
+		loc = list; 
+		prev = null; 
+	} 
+	public Node<T> getFoundNode(){ 
+		return foundNode; 
+	} 
+	public boolean find(obj toFind){ 
+		reset(); 
+		while(loc!=null){ 
+			/*if(((Fragment)((Node<T>)((SortedLinkedList<T>) this.list.getInfo()).list).getInfo()).getID()==toFind.getID()){ 
+				foundNode = loc; 
+				return true; 
+			} 
+			*/
+			step();
+		} 
+		return false; 
+	} 
+} 
