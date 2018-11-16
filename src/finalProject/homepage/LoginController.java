c3facdef439fcdbf30f50798ed556f0703fc8031
Vishnu <vishnuappalaraju@gmail.com>
2018-11-16 14:47:08 -0600

    lit

--- src/finalProject/homepage/LoginController.java
+++ src/finalProject/homepage/LoginController.java
@@ -3,9 +3,11 @@ package finalProject.homepage;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 public class LoginController implements Initializable {
@@ -14,8 +16,11 @@ public class LoginController implements Initializable {
 	   @FXML
 	   private TextField username;
 	   @FXML
-	   private TextField password;
+	   private PasswordField password;
+	   @Override
 	   public void initialize(URL location, ResourceBundle resources) {
 	   }
-	   public void 
+	   public void login(ActionEvent event) {
+		   
+	   }
 }
