d1b0610d7f864d3d861bef2377d10a7ad0c5e239
Unknown <20hussan@elmbrookstudents.org>
2018-11-16 08:42:54 -0600

    stuff

--- src/finalProject/homepage/RestaurantInfo.java
+++ src/finalProject/homepage/RestaurantInfo.java
@@ -1,20 +1,22 @@
 package finalProject.homepage;
 
 public class RestaurantInfo {
-private String name, address, phoneNo, cuisine, dineType, priceRange;
-private String[] hours;
-private LLStackReview reviews;
-private int avgRating;
-private RestaurantMenu menu;
-public RestaurantInfo(String name, String address, String phoneNo, String cuisine, String dineType, String priceRange, LLStackReview reviews, String[] hours, int rating, RestaurantMenu menu) {
-	this.name = name;
-	this.address = address;
-	this.phoneNo = phoneNo;
-	this.cuisine = cuisine;
-	this.dineType = dineType;
-	this.priceRange = priceRange;
-	this.reviews = reviews;
-	this.hours = hours;
-	this.menu = menu;
-}
+	private String name, address, phoneNo, cuisine, dineType, priceRange;
+	private String[] hours;
+	private LLStackReview reviews;
+	private int avgRating;
+	private RestaurantMenu menu;
+
+	public RestaurantInfo(String name, String address, String phoneNo, String cuisine, String dineType,
+			String priceRange, LLStackReview reviews, String[] hours, int rating, RestaurantMenu menu) {
+		this.name = name;
+		this.address = address;
+		this.phoneNo = phoneNo;
+		this.cuisine = cuisine;
+		this.dineType = dineType;
+		this.priceRange = priceRange;
+		this.reviews = reviews;
+		this.hours = hours;
+		this.menu = menu;
+	}
 }
--- src/finalProject/homepage/RestaurantMenu.java
+++ src/finalProject/homepage/RestaurantMenu.java
@@ -1,11 +1,14 @@
 package finalProject.homepage;
 
 public class RestaurantMenu {
-private SortedLinkedListMenu<FoodItem> entrees, sides, appetizers, drinks;
-public RestaurantMenu(){
-	entrees = new SortedLinkedListMenu<FoodItem>();
-	sides = new SortedLinkedListMenu<FoodItem>();
-	appetizers = new SortedLinkedListMenu<FoodItem>();
-	drinks = new SortedLinkedListMenu<FoodItem>();
-}
+	private SortedLinkedListMenu<FoodItem> entrees, sides, appetizers, drinks;
+
+	public RestaurantMenu(SortedLinkedListMenu<FoodItem> entrees, SortedLinkedListMenu<FoodItem> sides,
+			SortedLinkedListMenu<FoodItem> appetizers, SortedLinkedListMenu<FoodItem> drinks) {
+		this.entrees = entrees;
+		this.sides = sides;
+		this.appetizers = appetizers;
+		this.drinks = drinks;
+	}
+
 }
