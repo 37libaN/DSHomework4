f9b4465974499766826fe817d99740f1b1718eea
Vishnu <vishnuappalaraju@gmail.com>
2018-11-18 14:04:10 -0600

    nabil don't do anything

--- src/finalProject/homepage/LoginController.java
+++ src/finalProject/homepage/LoginController.java
@@ -31,6 +31,6 @@ public class LoginController implements Initializable {
 	   public void login(ActionEvent event) throws IOException {
 		   System.out.println(username.getText());
 		   System.out.println(password.getText());
-		   Login.getInstance().loginUser(username.getText(), password.getText());
+		   RestaurantAdvisor.getInstance().loginUser(username.getText(), password.getText());
 	   }
 }
