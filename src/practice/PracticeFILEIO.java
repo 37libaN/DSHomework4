33259067983c2b0f9415f78dd6c6d7829f3fa50f
Vishnu <vishnuappalaraju@gmail.com>
2018-11-30 11:32:40 -0600

    dxfcgdgdg

---
+++ src/practice/PracticeFILEIO.java
@@ -0,0 +1,15 @@
+package practice;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+public class PracticeFILEIO {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		//File file = new File(  );
+		//Scanner input = new Scanner();
+		//String str = input.nextLine();
+		//System.out.println(str);
+	}
+
+}
