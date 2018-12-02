bebd03cd35ae3b698319145dec33d69893c76f39
Vishnu <vishnuappalaraju@gmail.com>
2018-12-02 14:08:35 -0600

    br

--- src/finalProject/homepage/RestaurantAdvisor.java
+++ src/finalProject/homepage/RestaurantAdvisor.java
@@ -55,7 +55,7 @@ public class RestaurantAdvisor extends Application {
 		BinSearchTreeLogin<LoginInfo> loginDatabase = new BinSearchTreeLogin<LoginInfo>();
 		// C:/Users/liban/Desktop/loginData.txt
 		// User: 123hibob password: jshihi
-		File loginFile = new File("C:/Users/liban/Desktop/loginData.txt");
+		File loginFile = new File("C:/Users/vishn/Desktop/loginData.txt");
 		Scanner inputFile = new Scanner(loginFile);
 		while (inputFile.hasNextLine()) {
 			LoginInfo currLogin = new LoginInfo();
@@ -115,7 +115,7 @@ public class RestaurantAdvisor extends Application {
 
 	public static RestaurantInfo getRestaurantInfo() throws FileNotFoundException {
 		// C:/Users/vishn/Desktop/restaurantData.txt
-		File loginFile = new File("C:/Users/liban/Desktop/singleRestaurantData.txt");
+		File loginFile = new File("C:/Users/vishn/Desktop/singleRestaurantData.txt");
 		Scanner inputFile = new Scanner(loginFile);
 		String name = inputFile.nextLine();
 		String address = inputFile.nextLine();
@@ -143,7 +143,7 @@ public class RestaurantAdvisor extends Application {
 
 	public SortedLinkedList<RestaurantReviews> getAllReviews() throws FileNotFoundException {// returns all reviews 
 		SortedLinkedList<RestaurantReviews> allReviews = new SortedLinkedList<RestaurantReviews>(); 
-		File loginFile = new File("C:/Users/liban/Desktop/reviewData.txt"); 
+		File loginFile = new File("C:/Users/vishn/Desktop/reviewData.txt"); 
 		Scanner inputFile = new Scanner(loginFile); 
 		String check = ""; 
 		String check2 = ""; 
@@ -187,7 +187,7 @@ public class RestaurantAdvisor extends Application {
 
 	public SortedLinkedList<RestaurantInfo> getAllRestaurants() throws FileNotFoundException {// returns all restaurants
 		SortedLinkedList<RestaurantInfo> allRestaurants = new SortedLinkedList<RestaurantInfo>();
-		File loginFile = new File("C:/Users/liban/Desktop/restaurantData.txt");
+		File loginFile = new File("C:/Users/vishn/Desktop/restaurantData.txt");
 		Scanner inputFile = new Scanner(loginFile);
 		while (inputFile.hasNextLine()) {// go through file for restaurant data
 			String name = inputFile.nextLine();
--- src/finalProject/homepage/Writer.java
+++ src/finalProject/homepage/Writer.java
@@ -9,7 +9,7 @@ public class Writer {
 	private static PrintWriter reviewOutFile;
 
 	public static void writeRestaurant(RestaurantInfo restaurant) throws IOException {
-		PrintWriter outFile = new PrintWriter(new FileWriter("C:/Users/liban/Desktop/singleRestaurantData.txt"));
+		PrintWriter outFile = new PrintWriter(new FileWriter("C:/Users/vishn/Desktop/singleRestaurantData.txt"));
 		outFile.println(restaurant.getName());
 		outFile.println(restaurant.getAddress());
 		outFile.println(restaurant.getPhoneNo());
@@ -33,7 +33,7 @@ public class Writer {
 
 	public static void writeReview(String review, String rating) throws IOException {
 		SortedLinkedList<RestaurantReviews> allReviews = RestaurantAdvisor.getInstance().getAllReviews();
-		PrintWriter outFile = new PrintWriter(new FileWriter("C:/Users/liban/Desktop/reviewData.txt"));
+		PrintWriter outFile = new PrintWriter(new FileWriter("C:/Users/vishn/Desktop/reviewData.txt"));
 		RestaurantInfo restaurant = RestaurantAdvisor.getInstance().getRestaurantInfo();
 		if (allReviews.findReview(restaurant.getName())) {
 			RestaurantReviews thisRestaurantReviews = allReviews.getFoundNode().getInfo();
